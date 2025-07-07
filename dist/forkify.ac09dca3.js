55e7db2487b6065b9e51e99e443821a207c1e6ae
reyesgmary52 <reyesgmary52@gmail.com>
2025-07-07 08:52:58 -0600

    gitignoresecondfile

--- dist/forkify.ac09dca3.js
+++
@@ -1,2 +0,0 @@
-let recipeContainer=document.querySelector(".recipe"),timeout=function(e){return new Promise(function(o,t){setTimeout(function(){t(Error(`Request took too long! Timeout after ${e} second`))},1e3*e)})};
-//# sourceMappingURL=forkify.ac09dca3.js.map
