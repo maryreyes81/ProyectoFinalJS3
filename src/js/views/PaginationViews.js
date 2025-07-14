a7bbd6789195d5f8d4d2cbe9224e8b1e5c8fc285
reyesgmary52 <reyesgmary52@gmail.com>
2025-07-14 12:11:03 -0600

    Avance actividad semana  4

---
+++ src/js/views/PaginationViews.js
@@ -0,0 +1,51 @@
+import icons from "url:../../img/icons.svg";
+import View from "./View.js";
+
+class PaginationView extends View {
+  _parentElement = document.querySelector(".pagination");
+
+  _generateMarkup() {
+    const curPage = this._data.page;
+    const numPages = Math.ceil(
+      this._data.results.length / this._data.resultsPerPage
+    );
+    if (curPage === 1 && numPages > 1) {
+      return `
+        <button class="btn--inline pagination__btn--next">
+            <span>{curPage + 1}</span>
+            <svg class="search__icon">
+              <use href="${icons}#icon-arrow-right"></use>
+            </svg>
+          </button>
+        `;
+    }
+    if (curPage === numPages > 1) {
+      return `
+<button class="btn--inline pagination__btn--prev">
+            <svg class="search__icon">
+              <use href="${icons}#icon-arrow-left"></use>
+            </svg>
+            <span>{curPage - 1}</span>
+          </button>
+`;
+    }
+    if (curPage < numPages && curPage > 1) {
+      return `
+<button class="btn--inline pagination__btn--next">
+            <span>{curPage + 1}</span>
+            <svg class="search__icon">
+              <use href="${icons}#icon-arrow-right"></use>
+            </svg>
+          </button>
+
+<button class="btn--inline pagination__btn--prev">
+            <svg class="search__icon">
+              <use href="${icons}#icon-arrow-left"></use>
+            </svg>
+            <span>{curPage - 1}</span>
+          </button>
+`;
+    }
+    return "";
+  }
+}
