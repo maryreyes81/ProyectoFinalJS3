277c601c0ea903b589268254a5bc526ec9150729
reyesgmary52 <reyesgmary52@gmail.com>
2025-07-21 09:16:50 -0600

    x

--- src/js/views/RecipeView.js
+++ src/js/views/RecipeView.js
@@ -1,5 +1,5 @@
 import icons from "url:../../img/icons.svg";
-import fracty from "fracty";
+import Fraction from "fraction.js";
 import View from "./View.js";
 
 class RecipeView extends View {
@@ -70,8 +70,9 @@ class RecipeView extends View {
   _generateMarkup() {
     return `
     <figure class="recipe__fig">
-          <img src="${this._data.image}" alt="${this._data.title
-      }" class="recipe__img" />
+          <img src="${this._data.image}" alt="${
+      this._data.title
+    }" class="recipe__img" />
           <h1 class="recipe__title"> <span>
           ${this._data.title}</span>
           </h1>
@@ -82,8 +83,9 @@ class RecipeView extends View {
             <svg class="recipe__info-icon">
               <use href="${icons}#icon-clock"></use>
             </svg>
-            <span class="recipe__info-data recipe__info-data--minutes">${this._data.cookTime
-      }</span>
+            <span class="recipe__info-data recipe__info-data--minutes">${
+              this._data.cookTime
+            }</span>
             <span class="recipe__info-text">minutes</span>
           </div>
 
@@ -91,8 +93,9 @@ class RecipeView extends View {
             <svg class="recipe__info-icon">
               <use href="${icons}#icon-users"></use>
             </svg>
-            <span class="recipe__info-data recipe__info-data--people">${this._data.servings
-      }</span>
+            <span class="recipe__info-data recipe__info-data--people">${
+              this._data.servings
+            }</span>
             <span class="recipe__info-text">servings</span>
 
             <div class="recipe__info-buttons">
@@ -124,21 +127,22 @@ class RecipeView extends View {
         <div class="recipe__ingredients">
           <h2 class="heading--2">Recipe ingredients</h2>
           <ul class="recipe__ingredient-list">${this._data.ingredients
-        .map((ing) => {
-          return `
+            .map((ing) => {
+              return `
             <li class="recipe__ingredient">
             <svg class="recipe__icon">
                 <use href="${icons}#icon-check"></use>
               </svg>
-            <div class="recipe__quantity">${ing.quantity ? fracty(ing.quantity).toString() : ""
+            <div class="recipe__quantity">${
+              ing.quantity ? fracty(ing.quantity).toString() : ""
             }</div>
             <div class="recipe__description">
              <span class="recipe__unit">${ing.unit}</span>
              ${ing.description}
               </div>
             </li> `;
-        })
-        .join("")}
+            })
+            .join("")}
             </ul>
             </div>
 
@@ -147,8 +151,9 @@ class RecipeView extends View {
           <h2 class="heading--2">How to cook it</h2>
           <p class="recipe__directions-text">
             This recipe was carefully designed and tested by
-            <span class="recipe__publisher">${this._data.publisher
-      }</span>. Please check out
+            <span class="recipe__publisher">${
+              this._data.publisher
+            }</span>. Please check out
             directions at their website.
           </p>
           <a
@@ -165,5 +170,3 @@ class RecipeView extends View {
 }
 
 export default new RecipeView();
-
-
