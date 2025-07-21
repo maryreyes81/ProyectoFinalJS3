843cf63dbe904ce067da6cb41c002f9e7c27e783
reyesgmary52 <reyesgmary52@gmail.com>
2025-07-21 09:10:01 -0600

    se actualiza fraction

--- src/js/view/RecipeView.js
+++ src/js/view/RecipeView.js
@@ -1,5 +1,5 @@
 import icons from "url:../../img/icons.svg";
-import Fraction from 'fraction.js';
+import Fraction from "fraction.js";
 import View from "./View.js";
 
 class RecipeView extends View {
@@ -133,7 +133,9 @@ class RecipeView extends View {
             <svg class="recipe__icon">
                 <use href="${icons}#icon-check"></use>
               </svg>
-            <div class="recipe__quantity">${ing.quantity ? new Fraction(ing.quantity).toFraction(true) : ''}</div>
+            <div class="recipe__quantity">${
+              ing.quantity ? new Fraction(ing.quantity).toFraction(true) : ""
+            }</div>
             <div class="recipe__description">
              <span class="recipe__unit">${ing.unit}</span>
              ${ing.description}
